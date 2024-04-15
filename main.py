778bd7eb71fa130f276b60f0263446105caa8948
shubhanshu-easemyai <shubhanshu@easemyai.com>
2024-04-15 16:09:51 +0530

    fixed ROI settings over-right issue

--- main.py
+++ main.py
@@ -782,7 +782,10 @@ class DataProcessor:
                         logger.debug(max_people_count)
                         # logger.debug(self.total_detection_past_cache[_id])
                         logger.debug(self.total_detection_past_cache)
-                        if not min_people_count <= self.total_detection_past_cache[_id] <= max_people_count:
+
+                        # if _id not in self.total_detection_past_cache[_id] 
+
+                        if not min_people_count <= total_detection_count <= max_people_count:
                             logger.debug(self.total_detection_past_cache[_id])
                             logger.debug(total_detection_count)
                             self.total_detection_past_cache[_id] = total_detection_count
@@ -800,6 +803,25 @@ class DataProcessor:
                         if not min_people_count <= total_detection_count <= max_people_count:
                             self.total_detection_past_cache[_id] = total_detection_count
                             logger.debug(self.total_detection_past_cache[_id])
+
+                        # if not min_people_count <= self.total_detection_past_cache[_id] <= max_people_count:
+                        #     logger.debug(self.total_detection_past_cache[_id])
+                        #     logger.debug(total_detection_count)
+                        #     self.total_detection_past_cache[_id] = total_detection_count
+                        #     post_process(
+                        #         connector=connector,
+                        #         storage_path=self.image_storage_path,
+                        #         alert_schema=copy.deepcopy(self.alert_metadata),
+                        #         index=_id,
+                        #         detected_objects=copy.deepcopy(self.detected_objects[_id]),
+                        #         key=key,
+                        #         headers=source_details,
+                        #         transaction_id=transaction_id,
+                        #         **data,
+                        #     )
+                        # if not min_people_count <= total_detection_count <= max_people_count:
+                        #     self.total_detection_past_cache[_id] = total_detection_count
+                        #     logger.debug(self.total_detection_past_cache[_id])
 
                 else:
                     logger.debug(f"No detections found for _id: {_id}")
